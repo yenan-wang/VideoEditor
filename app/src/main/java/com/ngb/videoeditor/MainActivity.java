a40f380d73a8464b6f64ed05abf7c5ad8cf2a5a2
yenan-wang <3428141843@qq.com>
2021-01-09 16:16:23 +0800

    [VideoEditor][1/1]{ 首次提交工程 } 适用范围：{All} 准入id：{无} 分析：{无} 方案：{无} 风险及影响：{无} 测试建议：{} 其它：{}

---
+++ app/src/main/java/com/ngb/videoeditor/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ngb.videoeditor;
+
+import android.os.Bundle;
+
+import com.ngb.common.BaseCommonActivity;
+
+public class MainActivity extends BaseCommonActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
